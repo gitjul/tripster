85ff4cebbac21a28cf37189c4f3f562cd69d16fe
Julia Jacobs <gjulmail@gmail.com>
2012-01-07 19:42:26 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tripster::Application.config.secret_token = '870f566e39f19be27a8286ea92436ac3005d145e812671979a0e4a7c9fc1197e3ff16d6e26f9594bba882003951147d215f5e1383f4854aaaaa1e4249e4a0fe3'
