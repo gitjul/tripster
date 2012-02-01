8a4e9589acf4402e4de8892e7b79b73d11a27563
Julia Jacobs <gjulmail@gmail.com>
2012-02-01 15:20:24 +0100

    Added e-mail validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ActiveRecord::Base
     config.require_password_confirmation = true
     config.validate_email_field = false
   end
+  validates :email, :allow_blank => true,
+                    :length => {:minimum => 3, :maximum => 254},
+                    :uniqueness => true,
+                    :format => {:with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i}
 end
