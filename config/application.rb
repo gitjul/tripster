42cfd34223caa79712993a0bb9c128cbee9cf713
Julia Jacobs <gjulmail@gmail.com>
2012-02-05 16:32:07 +0100

    Hack fix to automatic creating of field_with_error divs

--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,8 @@ module Tripster
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    # HACK to fix field_with_error
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| "#{html_tag}".html_safe }
   end
 end
